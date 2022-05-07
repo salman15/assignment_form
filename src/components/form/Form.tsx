12672a7836945845cd3ca622207be5df6a39ab4e
salman <salmannova@gmail.com>
2022-05-07 13:17:57 +0200

    added form component

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from "react";
+
+interface IForm {
+  children: ReactNode;
+}
+
+const Form: FC<IForm> = ({ children }) => {
+  return (
+    <form className="rounded">
+      {children}
+    </form>
+  );
+};
+
+export default Form;
